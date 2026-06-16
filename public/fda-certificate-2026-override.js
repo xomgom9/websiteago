bda78a5b8ab4daacbb2c720151ce3ee891561a0f
xomgom9 <ngochuy8d@gmail.com>
2026-06-16 22:00:17 +0700

    Replace FDA certificate display with uploaded image asset

--- public/fda-certificate-2026-override.js
+++ public/fda-certificate-2026-override.js
@@ -1,15 +1,34 @@
 (() => {
-  const FDA_2026_SRC = "/assets/fda-certificate-2026-image.webp?v=20260616-2";
+  const FDA_2026_SRC = "/assets/fda-certificate-2026-image.webp?v=20260616-uploaded";
+
+  function applyImageStyle(img) {
+    img.src = FDA_2026_SRC;
+    img.alt = "FDA Certification of Registration 2026";
+    img.loading = "lazy";
+    img.decoding = "async";
+    img.dataset.fda2026 = "true";
+    img.style.display = "block";
+    img.style.width = "100%";
+    img.style.height = "auto";
+    img.style.objectFit = "contain";
+  }
 
   function swapFdaCertificate() {
-    const images = document.querySelectorAll('.cert-image-fda img, img[alt*="FDA" i]');
+    const images = document.querySelectorAll('.cert-image-fda img, img[alt*="FDA" i], img[src*="fda" i], img[src*="certificate" i]');
     let swapped = false;
 
     images.forEach((img) => {
-      if (img.dataset.fda2026 === "true") return;
-      img.src = FDA_2026_SRC;
-      img.alt = "FDA Certification of Registration 2026";
-      img.dataset.fda2026 = "true";
+      if (img.dataset.fda2026 === "true" && img.src.includes(FDA_2026_SRC)) return;
+      applyImageStyle(img);
+      swapped = true;
+    });
+
+    const certContainers = document.querySelectorAll('.cert-image-fda, [class*="fda" i], [class*="certificate" i]');
+    certContainers.forEach((container) => {
+      if (container.querySelector('img[data-fda2026="true"]')) return;
+      const img = document.createElement("img");
+      applyImageStyle(img);
+      container.appendChild(img);
       swapped = true;
     });
 
